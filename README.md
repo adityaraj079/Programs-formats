b8a593b903b734c09c3f2a46a9a66076fe6f320e
Aditya <rajaditya079@gmail.com>
2021-04-03 14:38:55 +0530

    C++ format done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Programs-formats
+
+S.No.  |  Link  |  Language  |  Comments
+-----  |  ----  |  --------  |  --------
+1  |  Link  |  C++  |  This is used for CP problems
---
+++ c++/format.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (ll i=0; i<n; i++)
+#define rep1(i,n) for(ll i=1; i<=n; i++)
+#define pb push_back
+#define lc lower_bound
+#define beg() begin()
+#define sp <<":"<<
+#define f first
+#define s second 
+#define FastIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main()
+{
+	FastIO
+	
+	/* rep(i,N); 
+	{
+		
+	}
+	This is loop from 0 to N-1
+	*/
+	
+	/* rep1(i,N); 
+	{
+		
+	}
+	This is loop from 1 to N
+	*/
+}
