4886f47a15ef82990afde557de6c9b0b52c0adb4
Aditya Raj <rajaditya079@gmail.com>
2021-10-01 10:29:35 +0530

    CP optimization added

--- c++/format.cpp
+++ c++/format.cpp
@@ -1,20 +1,21 @@
 #include <bits/stdc++.h>
+#pragma GCC optimize ("03")              //For CP optimize
+#pragma GCC target ("sse4")              //For CP optimize
 using namespace std;
-typedef long long ll;
+typedef long long ll;                   //Define ll for long long int
 #define rep(i,n) for (ll i=0; i<n; i++)
 #define rep1(i,n) for(ll i=1; i<=n; i++)
-#define pb push_back
-#define lc lower_bound
-#define beg() begin()
-#define sp <<":"<<
-#define f first
-#define s second 
-#define FastIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define FastIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);          //Define fastio so that the do not search for cin or cout each line
 
 int main()
 {
 	FastIO
-	
+	ll t; cin>>t;
+	while(t--)
+	{
+		ll n; cin>>n;
+		
+	}
 	/* rep(i,N); 
 	{
 		
